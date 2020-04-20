93534ddd4456a41b8e1b8c1114856e844469e67b
Varun Bhardwaj <varunbhardwaj@tutanota.com>
2020-04-20 18:25:55 +0530

    remote control

---
+++ Light.java
@@ -0,0 +1,13 @@
+public class Light{
+    String location = "";
+
+	public Light(String location) {
+		this.location = location;
+	}
+    public void on(){
+        System.out.println("Light is On");
+    }
+    public void off(){
+        System.out.println("Light is Off");
+    }
+}
